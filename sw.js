6e431be4fcc8ddc00fe035bba4e94c648c27e3de
VOPskf <clementpouyer@gmail.com>
2025-05-20 22:26:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'anatomy-annotator-v1';
 const urlsToCache = [
   '/',
-  '/Code_App.html',
+  '/index.html',
   '/manifest.json',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
